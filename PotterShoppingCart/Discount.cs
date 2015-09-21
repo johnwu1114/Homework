03970411995be7a8951a471fb07071718f05def5
johnwu1114 <me@johnwu.cc>
2015-09-21 15:36:50 +0800

    Passed sixth test case

--- PotterShoppingCart/Discount.cs
+++ PotterShoppingCart/Discount.cs
@@ -8,17 +8,31 @@ namespace PotterShoppingCart
     {
         public int GetDiscount(List<Order> order)
         {
-            var sameBookGroup = order.GroupBy(g => g.BookName);
-            var serialGroup = sameBookGroup
-                .Select((n, i) => new { 
-                    Group = i / sameBookGroup.Count(), 
-                    Value = n 
-                })
+            //先將同類書籍分類
+            var sameBookGroup = order
+                .GroupBy(g => g.BookName);
+
+            //把分類後的書籍加入索引值，再依索引值做為新的群組
+            var bookList = sameBookGroup.Select(g => new
+                {
+                    Value = g.Select((val, idx) => new
+                    {
+                        Index = idx,
+                        Value = val
+                    })
+                }).SelectMany(g => g.Value.Select(v => new
+                {
+                    Group = v.Index,
+                    Value = v.Value
+                }));
+
+            //把各群組計算總合
+            var serialGroup = bookList
                 .GroupBy(g => g.Group)
                 .Select(g => new
                 {
-                    Count = g.Sum(s => s.Value.Count()),
-                    Sum = g.Sum(s => s.Value.Sum(o => o.Price))
+                    Count = g.Count(),
+                    Sum = g.Sum(o => o.Value.Price)
                 });
 
             return serialGroup.Sum(o => GetDiscount(o.Sum, o.Count));
@@ -30,10 +44,13 @@ namespace PotterShoppingCart
             {
                 case 2:
                     return Convert.ToInt32(sumPrice * 0.95);
+
                 case 3:
                     return Convert.ToInt32(sumPrice * 0.90);
+
                 case 4:
                     return Convert.ToInt32(sumPrice * 0.80);
+
                 case 5:
                     return Convert.ToInt32(sumPrice * 0.75);
             }
